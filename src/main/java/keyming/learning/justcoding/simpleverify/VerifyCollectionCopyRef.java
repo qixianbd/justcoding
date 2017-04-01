99e7fb1428a217d8731b14eea6c48dc9030252f0
qixianbd <keymingstar@gmail.com>
2017-04-01 23:17:40 +0800

    add container element assignment vefiry.

--- src/main/java/keyming/learning/justcoding/simpleverify/VerifyCollectionCopyRef.java
+++ src/main/java/keyming/learning/justcoding/simpleverify/VerifyCollectionCopyRef.java
@@ -20,16 +20,26 @@ public class VerifyCollectionCopyRef {
 		
 		// test with ArrayList.
 		Person p = Person.builder().name("Tom").address("South").sex(Sex.MALE).build();
-		List<Person> personList = new ArrayList<Person>(1);
+		ArrayList<Person> personList = new ArrayList<Person>(2);
 		personList.add(p);
 		personList.get(0).setName("Mike");
 		Assert.assertEquals("Mike", p.getName());
 		
 		// test with Array.
-		Person[] personArray = new Person[1];
+		Person[] personArray = new Person[2];
 		personArray[0] = p;
 		personArray[0].setSex(Sex.FEMALE);
 		Assert.assertEquals(Sex.FEMALE, p.getSex());
 		Assert.assertEquals(Sex.FEMALE, personList.get(0).getSex());
+		
+		// 使用ArrayList.set方法来重新设置对象的引用。
+		Person p2 = Person.builder().name("Lily").address("USA").sex(Sex.FEMALE).build();
+		personList.set(0, p2);
+		Assert.assertEquals("Lily", personList.get(0).getName());
+		
+		// for array, use [i] 来返回左值引用。 
+		Person p3 = Person.builder().name("Lucy").address("USA").sex(Sex.FEMALE).build();
+		personArray[0] = p3;
+		Assert.assertEquals("Lucy", personArray[0].getName());
 	}
 }
